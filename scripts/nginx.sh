1075e295b7644599cb6fb476f5db446569fe77e9
paranigit <parani.gobi@gmail.com>
2024-02-01 23:55:29 -0600

    initial project setup after fork

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,8 +2,8 @@
 
 sudo systemctl daemon-reload
 systemctl restart gunicorn.service
-sudo cp /nginx/nginx.conf /etc/nginx/sites-available/ci-cd-django
-sudo ln -s /etc/nginx/sites-available/ci-cd-django /etc/nginx/sites-enabled
+sudo cp /nginx/nginx.conf /etc/nginx/sites-available/djLifeConnect
+sudo ln -s /etc/nginx/sites-available/djLifeConnect /etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl restart nginx
 sudo ufw delete allow 8000
